5e4253ef833f3b8cb657ba6916989c27de609d93
gerardimitri <80934723+gerardimitri@users.noreply.github.com>
2022-09-19 23:01:03 -0300

    TERMINÉEEEE

--- resolver.py
+++ resolver.py
@@ -21,6 +21,7 @@ from dnslib import DNSRecord, DNSHeader, DNSQuestion
 SOCKET_HOST = "localhost"
 SOCKET_PORT = 5300
 BUFF_SIZE = 4096
+DEBUG = True
 
 cache = {}
 dom_list = [None]*100
@@ -84,7 +85,7 @@ def DNSresolver(domain_name, server_address=("8.8.8.8", 53)):
                ar_type = QTYPE.get(first_additional_record.rclass) 
                if ar_type == 'A': # si el tipo es 'A' (Address)
                     name_server = str(first_additional_record.rname)  # nombre de dominio             
-                    server_ip = first_additional_record.rdata  # IP asociada
+                    #server_ip = first_additional_record.rdata  # IP asociada
 
           if number_of_authority_elements > 0 and name_server == "":
                authority_section_list = dnslib_reply.auth  # contiene un total de number_of_authority_elements
@@ -97,72 +98,84 @@ def DNSresolver(domain_name, server_address=("8.8.8.8", 53)):
                     elif isinstance(authority_section_0_rdata, dnslib.dns.NS): # si en vez de SOA recibimos un registro tipo NS
                          name_server = str(authority_section_0_rdata)  # entonces authority_section_0_rdata contiene el nombre de dominio del primer servidor de nombre de la lista
 
-          if name_server != "":
-               print(f"(debug) consultando el NS de {part} en {name_server}")
-          else:
+          if name_server != "" and DEBUG:
+               print(f"(debug) consultando el NS de {part} en {server_address[0]}")
+          elif DEBUG:
                print(f"(debug) no se pudo resolver {part}")
 
           dnslib_reply = send_dns_message(server_address[0], server_address[1], qname = name_server)
           number_of_authority_elements = dnslib_reply.header.auth
           number_of_answer_elements = dnslib_reply.header.a
-          number_of_additional_elements = dnslib_reply.header.ar
+          number_of_additional_elements = dnslib_reply.header.ar        
 
           if number_of_answer_elements > 0 and server_ip == "":
                first_answer = dnslib_reply.get_a()
                server_ip = str(first_answer.rdata)
 
-          elif number_of_additional_elements > 0 and server_ip == "":
+          if number_of_additional_elements > 0 and server_ip == "":
                additional_records = dnslib_reply.ar 
                first_additional_record = additional_records[0]
                ar_type = QTYPE.get(first_additional_record.rclass) 
                if ar_type == 'A': # si el tipo es 'A' (Address)           
                     server_ip = str(first_additional_record.rdata)  # IP asociada
 
-          if server_ip != "":
+          if server_ip != "" and DEBUG:
                print(f"(debug) consultando {name_server} en {server_ip}")
-          else:
-               print(f"(debug) no se pudo resolver {name_server}")
+          elif DEBUG:
+               server_ip = DNSresolver(name_server, ("8.8.8.8", 53))
+               if server_ip == "" and DEBUG:
+                    print(f"(debug) no se pudo resolver {name_server}")
+                    return ""
           server_address = (server_ip, 53)
 
      print(f"{domain_name} -> {server_ip}")
      return server_address[0]
-          
+
+def resolverWithCache(qname):
+     # CACHE
+     global COUNT
+     global cache
+     global dom_list
+     dom_list[COUNT%100] = qname
+     cache_counted = {i:dom_list.count(i) for i in dom_list}
+     cache_counted = sorted(cache_counted, key = cache_counted.get, reverse = True)
+     top10 = cache_counted[:10]
+     if qname in cache:
+          print("Cache hit")
+          ip_answer = cache[qname]
+     else:
+          print("Cache miss")
+          cache.pop(qname, None)
+          ip_answer = str(DNSresolver(qname))
+          if qname in top10:
+               cache[qname] = ip_answer
+     COUNT += 1
+     return ip_answer
+
                
 while True:
      resolver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
      resolver_socket.bind((SOCKET_HOST, SOCKET_PORT))
      received, client_address = resolver_socket.recvfrom(BUFF_SIZE)
 
-     qname = str(sys.argv[1])
+     qname_list = []
+     for arg in sys.argv[1:]:
+          qname_list.append(arg)
 
      received_copy = parse_dns_message(received)
      print("Received: ", received)
      print("Parsed Received: ", str(received_copy))
 
-     # # CACHE
-     # dom_list[COUNT%100] = qname
-     # cache_counted = {i:dom_list.count(i) for i in dom_list}
-     # cache_counted = sorted(cache_counted, key = cache_counted.get, reverse = True)
-     # top10 = cache_counted[:10]
-     # if qname in cache:
-     #      print("Cache hit")
-     #      ip_answer = cache[qname]
-     # else:
-     #      print("Cache miss")
-     #      cache.pop(qname, None)
-     #      ip_answer = str(DNSresolver(qname))
-     #      if qname in top10:
-     #           cache[qname] = ip_answer
-     # COUNT += 1
+     for qname in qname_list:
+          # CACHE
+          ip_answer = resolverWithCache(qname)
 
-     ip_answer = str(DNSresolver(qname))
-     received_copy.add_answer(RR(qname, QTYPE.A, rdata=A(ip_answer)))
-     # print("Parsed: ", parse_dns_message(received))
-     # print("Packed: ", pack_dns_message(parse_dns_message(received)))
+          # ip_answer = str(DNSresolver(qname))
+          received_copy.add_answer(RR(qname, QTYPE.A, rdata=A(ip_answer)))
+          # print("Parsed: ", parse_dns_message(received))
+          # print("Packed: ", pack_dns_message(parse_dns_message(received)))
 
-     # print("RESOLVER: " + str(DNSresolver("www.uchile.cl")))
-     resolver_socket.sendto(pack_dns_message(received_copy), client_address)
-     print("RESOLVER: " + ip_answer)
-     
-
+          # print("RESOLVER: " + str(DNSresolver("www.uchile.cl")))
+          resolver_socket.sendto(pack_dns_message(received_copy), client_address)
+          print("RESOLVER: " + ip_answer)
      break
